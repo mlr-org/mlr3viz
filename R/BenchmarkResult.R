549dfda3175734224711c2faafe0f47a284ab088
Michel Lang <michellang@gmail.com>
2018-11-28 15:20:45 +0100

    simplified bmr plot

--- R/BenchmarkResult.R
+++ R/BenchmarkResult.R
@@ -4,9 +4,8 @@
 #' Generates plots for [mlr3::BenchmarkResult].
 #'
 #' @param object \[[mlr3::BenchmarkResult]\].
-#' @param type \[character(1)\]: Type of the plot.
-#' @param by \[character(1)\]: Group plots by this variable.
-#' @param ... \[any\]: Additional arguments, passed down to the respective `geom`.
+#' @param measure \[[mlr3::Measure]\]. If NULL, defaults to first measure, i.e.
+#'  `object$measures$measure[[1]]`.
 #'
 #' @return [ggplot2::ggplot()] object.
 #' @export
@@ -20,21 +19,20 @@
 #' library(ggplot2)
 #' fortify(object)
 #' autoplot(object)
-autoplot.BenchmarkResult = function(object, type = "boxplot", by = "task_id", ...) {
-  assert_choice(by, c("learner_id", "task_id"))
-  x = setdiff(c("learner_id", "task_id"), by)
+autoplot.BenchmarkResult = function(object, measure = NULL) {
+  measure = measure %??% object$measures$measure[[1L]]
+  assert_measure(measure)
 
-  switch(type,
-    "boxplot" =
-      ggplot(object, aes_string(x, "performance")) + geom_boxplot() + facet_wrap(by),
-    stop("Unknown type")
-  )
+  ggplot(object, measure = measure, aes_string("learner_id", "performance")) +
+    geom_boxplot() + facet_wrap("task_id") + ylab(measure$id)
 }
 
 #' @export
-fortify.BenchmarkResult = function(model, data, ...) {
-  measure_ids = model$measures$measure_id
-  data = as.data.table(model)[, c("hash", "task_id", "learner_id", "resampling_id", measure_ids), with = FALSE]
-  melt(data, measure.vars = measure_ids,
-    variable.name = "measure", value.name = "performance")
+fortify.BenchmarkResult = function(model, data = NULL, measure = NULL) {
+  measure = measure %??% model$measures$measure[[1L]]
+  assert_measure(measure)
+
+  data = as.data.table(model)[, c("hash", "task_id", "learner_id", "resampling_id", measure$id), with = FALSE]
+  melt(data, measure.vars = measure$id,
+    variable.name = "measure_id", value.name = "performance")[get("measure_id") == measure$id]
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mlr3viz
+
+ [![Travis build status](https://travis-ci.org/mlr-org/mlr3viz.svg?branch=master)](https://travis-ci.org/mlr-org/mlr3viz)
+
 Visualizations for mlr3
