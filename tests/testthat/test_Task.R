957df5a5281e6cc59a2488b9d8cd1148953cdf12
Michel Lang <michellang@gmail.com>
2019-05-08 11:05:13 +0200

    renamed boston_housing task

--- tests/testthat/test_Task.R
+++ tests/testthat/test_Task.R
@@ -2,5 +2,5 @@ context("fortify.Task")
 
 test_that("fortify.Task", {
   expect_data_table(fortify(mlr_tasks$get("iris")), nrow = 150, ncol = 5)
-  expect_data_table(fortify(mlr_tasks$get("bh")))
+  expect_data_table(fortify(mlr_tasks$get("boston_housing")))
 })
