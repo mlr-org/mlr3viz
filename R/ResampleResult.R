13875838b8a7c219817013a06fe9d7254cd16233
Tobias Pielok <t.pielok@gmail.com>
2020-01-10 13:30:12 +0100

    Add plotpred2d

--- R/ResampleResult.R
+++ R/ResampleResult.R
@@ -67,6 +67,10 @@ autoplot.ResampleResult = function(object, type = "boxplot", measure = NULL, ...
       autoplot(precrec::evalmod(as_precrec(object)), curvetype = "prc", show_cb = TRUE)
     },
 
+    "pred2D" = {
+      plotpred2D(object, ...)
+    },
+    
     stopf("Unknown plot type '%s'", type)
   )
 }
@@ -80,4 +84,118 @@ fortify.ResampleResult = function(model, data, measure = NULL, ...) {
     variable.name = "measure_id", value.name = "performance")
 }
 
-
+plotpred2D = function(object, gridsize = 100, prob_alpha = TRUE,
+                      pointsize = 2,
+                      err_mark = "train",
+                      bg_cols = c("darkblue", "green", "darkred"),
+                      err_col = "white", err_size = pointsize,
+                      greyscale = FALSE) {
+  task = object$task
+  target_names = task$target_names
+  feature_names = task$feature_names
+  
+  data = task$data(task$row_ids)
+  features = task$data(task$row_ids, cols = feature_names)
+  
+  x1n = feature_names[1]
+  x2n = feature_names[2]
+  
+  x1 = features[, 1]
+  x2 = features[, 2]
+  
+  y = task$data(task$row_ids, cols = target_names)[[1]]
+  
+  grid = expand.grid(
+    seq(min(x1), max(x1), length.out = gridsize),
+    seq(min(x2), max(x2), length.out = gridsize)
+  )
+  
+  colnames(grid) = feature_names
+  
+  cmpl_lrn = object$learners[[1]]$clone()
+  cmpl_lrn$train(task)
+  train_pred = cmpl_lrn$predict(task)
+  y_hat = train_pred$response
+  
+  if (task$task_type == "classif" && "prob" %in% cmpl_lrn$predict_types) {
+    cmpl_lrn$predict_type = "prob"
+  }
+  
+  grid_pred = cmpl_lrn$predict_newdata(newdata = grid)
+  grid[, target_names] = grid_pred$response
+  
+  if (task$task_type == "classif") {
+    data$.err = if (err_mark == "train") {
+      y != y_hat
+    } else if (err_mark == "cv") {
+      y != object$prediction()$data$tab$response
+    } else {
+      TRUE
+    }
+    
+    if ("prob" %in% cmpl_lrn$predict_types && prob_alpha) {
+      grid$.prob.pred.class = apply(grid_pred$prob, 1, max)
+      p = ggplot(grid, aes_string(x = x1n, y = x2n))
+      p = p + geom_raster(
+        data = grid, mapping = aes_string(fill = target_names, alpha = ".prob.pred.class"),
+        show.legend = TRUE
+      ) + scale_fill_discrete(drop = FALSE)
+      p = p + scale_alpha(limits = range(grid$.prob.pred.class))
+    } else {
+      p = p + geom_raster(mapping = aes_string(fill = target_names))
+    }
+    
+    # print normal points
+    p = p + geom_point(
+      data = subset(data, !data$.err),
+      mapping = aes_string(x = x1n, y = x2n, shape = target_names), size = pointsize
+    )
+    # mark incorrect points
+    if (err_mark != "none" && any(data$.err)) {
+      p = p + geom_point(data = subset(data, data$.err),
+                         mapping = aes_string(x = x1n, y = x2n, shape = target_names),
+                         size = err_size + 1.5, show.legend = FALSE)
+      p = p + geom_point(data = subset(data, data$.err),
+                         mapping = aes_string(x = x1n, y = x2n, shape = target_names),
+                         size = err_size + 1, col = err_col, show.legend = FALSE)
+    }
+    # print error points
+    p = p + geom_point(data = subset(data, data$.err),
+                       mapping = aes_string(x = x1n, y = x2n, shape = target_names), size = err_size, show.legend = FALSE)
+    p = p + guides(alpha = FALSE)
+  } else if(task$task_type == "regr"){
+    # FIXME: color are not scaled correctly? can be improved?
+    # plot background from model / grid
+    p = ggplot(mapping = aes_string(x = x1n, y = x2n))
+    p = p + geom_raster(data = grid, mapping = aes_string(fill = target_names))
+    p = p + scale_fill_gradient2(low = bg_cols[1L], mid = bg_cols[2L], high = bg_cols[3L], space = "Lab")
+    # plot point, with circle and interior color for y
+    p = p + geom_point(data = data, mapping = aes_string(x = x1n, y = x2n, colour = target_names),
+                       size = pointsize)
+    p = p + geom_point(data = data, mapping = aes_string(x = x1n, y = x2n),
+                       size = pointsize, colour = "black", shape = 1)
+    # plot point, with circle and interior color for y
+    p = p + scale_colour_gradient2(low = bg_cols[1L], mid = bg_cols[2L], high = bg_cols[3L], space = "Lab")
+    p = p + guides(colour = FALSE)
+  }
+  
+  # set title
+  lrn.str = cln_lrn$id
+  
+  resamp_score = object$prediction()$score()
+  train_score = train_pred$score()
+  
+  # title = sprintf("%s: %s", lrn.str, paramValueToString(learner$par.set, learner$par.vals))
+  title = sprintf("%s", lrn.str)
+  title = sprintf("%s\nTrain: %s=%s; Resampl: %s=%s", title, 
+                  attr(resamp_score,"names"), as.character(round(resamp_score,2)),
+                  attr(train_score,"names"), as.character(round(train_score,2)))
+  
+  p = p + ggtitle(title)
+  
+  # deal with greyscale
+  if (greyscale) {
+    p = p + scale_fill_grey()
+  }
+  return(p)
+}
