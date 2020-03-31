e67b52889a826057ddd564967b7bd21c96a7e1b2
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-03-31 11:52:21 +0100

    Add autoplot.TaskDens

---
+++ R/TaskDens.R
@@ -0,0 +1,43 @@
+#' @title Plot for Density Tasks
+#'
+#' @description
+#' Generates plots for [mlr3proba::TaskDens].
+#'
+#' @param object ([mlr3proba::TaskDens]).
+#' @param type (`character(1)`):
+#'   Type of the plot. Available choices:
+#'   * `"dens"`: histogram density estimator (default) with [ggplot2::geom_histogram].
+#'   * `"freq"`: histogram frequency plot with [ggplot2::geom_histogram].
+#'   * `"overlay"`: histogram with overlaid density plot with [ggplot2::geom_histogram] and [ggplot2::geom_density].
+#'   * `"freqpoly"`: frequency polygon plot with [ggplot2::geom_freqpoly].
+#' @param ... (`any`):
+#'   Additional arguments, possibly passed down to the underlying plot functions.
+#' @return [ggplot2::ggplot()] object.
+#' @export
+#' @examples
+#' library(mlr3)
+#' library(mlr3proba)
+#' task = mlr_tasks$get("precip")
+#'
+#' head(fortify(task))
+#' autoplot(task, bins = 15)
+#' autoplot(task, type = "freq", bins = 15)
+#' autoplot(task, type = "overlay", bins = 15)
+#' autoplot(task, type = "freqpoly", bins = 15)
+autoplot.TaskDens = function(object, type = "dens", ...) {
+  assert_choice(type, c("dens", "freq", "overlay", "freqpoly"))
+
+  p = ggplot(data = object, aes_string(x = object$target_names),...)
+
+  if (type == "dens") {
+    p + geom_histogram(aes(y = ..density..), fill = "white", color = "black",...)
+  } else if (type == "freq") {
+    p + geom_histogram(fill = "white", color = "black", ...)
+  } else if (type == "overlay") {
+    p +
+      geom_histogram(aes(y = ..density..), colour = "black", fill = "white",...) +
+      geom_density(alpha = 0.2, fill = "#FF6666")
+  } else {
+    p + geom_freqpoly(...)
+  }
+}
---
+++ tests/testthat/test_TaskDens.R
@@ -0,0 +1,21 @@
+context("TaskSurv")
+
+
+test_that("autoplot.TaskDens", {
+  skip_if_not_installed("mlr3proba")
+
+  require_namespaces("mlr3proba")
+  task = mlr_tasks$get("precip")
+
+  p = autoplot(task, type = "dens")
+  expect_true(is.ggplot(p))
+
+  p = autoplot(task, type = "freq")
+  expect_true(is.ggplot(p))
+
+  p = autoplot(task, type = "overlay")
+  expect_true(is.ggplot(p))
+
+  p = autoplot(task, type = "freqpoly")
+  expect_true(is.ggplot(p))
+})
