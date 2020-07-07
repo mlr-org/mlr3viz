41eb5b593103ca7a7ae329a7d9d2a7eb6781416a
damirpolat <dpulatov@uwyo.edu>
2020-07-07 00:04:46 -0600

    fixed style

--- R/PredictionClust.R
+++ R/PredictionClust.R
@@ -2,9 +2,7 @@
 #'
 #' @description
 #' Generates plots for [mlr3cluster::PredictionClust], depending on argument `type`:
-#'
-#' * `"scatter"` (default): scatterplot with correlation values 
-#' and colored cluster assignments.
+#' * `"scatter"` (default): scatterplot with correlation values and cluster assignments.
 #'
 #' @param object ([mlr3cluster::PredictionClust]).
 #' @param task ([mlr3cluster::TaskClust]).
@@ -18,7 +16,7 @@
 #' library(mlr3)
 #' library(mlr3cluster)
 #' library(mlr3viz)
-#' 
+#'
 #' task = tsk("usarrests")
 #' learner = lrn("clust.kmeans", centers = 3)
 #' object = learner$train(task)$predict(task)
@@ -27,20 +25,20 @@
 #' autoplot(object, task)
 autoplot.PredictionClust = function(object, task, type = "scatter", ...) { # nolint
   assert_string(type)
-  
+
   switch(type,
-     "scatter" = {
-       require_namespaces("GGally")
-       
-       # merge features and partitions
-       plot.data = data.table(row_id = 1:nrow(task$data()), task$data())
-       plot.data = merge(object$data$tab, plot.data)
-       plot.data$row_id = NULL
-       plot.data$partition = factor(plot.data$partition)
-       
-       GGally::ggscatmat(plot.data, color = "partition", ...)
-     },
-     
-     stopf("Unknown plot type '%s'", type)
+    "scatter" = {
+      require_namespaces("GGally")
+
+      # merge features and partitions
+      plot.data = data.table(row_id = 1:nrow(task$data()), task$data())
+      plot.data = merge(object$data$tab, plot.data)
+      plot.data$row_id = NULL
+      plot.data$partition = factor(plot.data$partition)
+
+      GGally::ggscatmat(plot.data, color = "partition", ...)
+    },
+
+    stopf("Unknown plot type '%s'", type)
   )
 }
