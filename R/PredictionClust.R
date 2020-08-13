4e06b8135f154ba2e76672516cfb16b523fad0ea
damirpolat <dpulatov@uwyo.edu>
2020-08-13 13:32:40 -0600

    added silhouette plots for clust predictions

--- R/PredictionClust.R
+++ R/PredictionClust.R
@@ -3,7 +3,7 @@
 #' @description
 #' Generates plots for [mlr3cluster::PredictionClust], depending on argument `type`:
 #'
-#' * `"scatter"` (default): scatterplot with correlation values 
+#' * `"scatter"` (default): scatterplot with correlation values
 #' and colored cluster assignments.
 #'
 #' @param object ([mlr3cluster::PredictionClust]).
@@ -18,7 +18,7 @@
 #' library(mlr3)
 #' library(mlr3cluster)
 #' library(mlr3viz)
-#' 
+#'
 #' task = tsk("usarrests")
 #' learner = lrn("clust.kmeans", centers = 3)
 #' object = learner$train(task)$predict(task)
@@ -27,20 +27,46 @@
 #' autoplot(object, task)
 autoplot.PredictionClust = function(object, task, type = "scatter", ...) { # nolint
   assert_string(type)
-  
+
   switch(type,
      "scatter" = {
        require_namespaces("GGally")
-       
+
        # merge features and partitions
-       plot.data = data.table(row_id = 1:nrow(task$data()), task$data())
-       plot.data = merge(object$data$tab, plot.data)
-       plot.data$row_id = NULL
-       plot.data$partition = factor(plot.data$partition)
-       
-       GGally::ggscatmat(plot.data, color = "partition", ...)
+       data = data.table(row_id = 1:nrow(task$data()), task$data())
+       data = merge(object$data$tab, data)
+       data$row_id = NULL
+       data$partition = factor(data$partition)
+
+       GGally::ggscatmat(data, color = "partition", ...)
      },
-     
+
+     "sil" = {
+       require_namespaces("cluster")
+
+       # prepare data
+       d = dist(task$data())
+       widths = cluster::silhouette(object$data$tab$partition, d)
+
+       data = as.data.frame(unclass(widths))
+       data$cluster = as.factor(data$cluster)
+       data = data[order(data$cluster), ]
+       data$id = seq_len(nrow(data))
+
+       min.y = if(min(data$sil_width) < 0) min(data$sil_width) else 0
+       ggplot(data, aes(x = id, y = sil_width, fill = cluster)) +
+         geom_bar(stat = "identity", color = "black") +
+         ylim(min.y, 1) +
+         labs(x = NULL, y = "Silhouette Width",
+              title = sprintf("Average Silhouette Width: %s", round(mean(data$sil_width), 2))) +
+         theme(axis.text.y = element_blank(),
+               axis.ticks.y = element_blank(),
+               plot.title = element_text(hjust = 0.5),
+               axis.title.y = element_text(hjust = 0.5)) +
+         geom_hline(yintercept = round(mean(data$sil_width), 2), color = "red", linetype = "dashed") +
+         coord_flip()
+     },
+
      stopf("Unknown plot type '%s'", type)
   )
 }
