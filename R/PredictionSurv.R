afac7ea82641b467ff3563ec991de6e9b13999e7
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-12-05 21:44:15 +0000

    add dcalib to predictionsurv

--- R/PredictionSurv.R
+++ R/PredictionSurv.R
@@ -4,10 +4,14 @@
 #' Generates plots for [mlr3proba::PredictionSurv], depending on argument `type`:
 #'
 #' * `"calib"` (default): Calibration plot comparing the average predicted survival distribution
-#'   to a Kaplan-Meier prediction, this is *not* a comparison of a stratified `crank` or `lp`
-#'   prediction. `object` must have `distr` prediction. `geom_line()` is used for comparison split
-#'   between the prediction (`Pred`) and Kaplan-Meier estimate (`KM`). In addition labels are added
-#'   for the x (`T`) and y (`S(T)`) axes.
+#' to a Kaplan-Meier prediction, this is *not* a comparison of a stratified `crank` or `lp`
+#' prediction. `object` must have `distr` prediction. `geom_line()` is used for comparison split
+#'  between the prediction (`Pred`) and Kaplan-Meier estimate (`KM`). In addition labels are added
+#'  for the x (`T`) and y (`S(T)`)
+#' axes.
+#' * `"dcalib"`: Distribution calibration plot. A model is D-calibrated if X% of deaths occur before
+#' the X/100 quantile of the predicted distribution, e.g. if 50% of observations die before their
+#' predicted median survival time. A model is D-calibrated if the resulting plot lies on x = y.
 #'
 #' @param object ([mlr3proba::PredictionSurv]).
 #' @template param_type
@@ -18,8 +22,17 @@
 #' @param times (`numeric()`) \cr
 #'   If `type = "calib"` then `times` is the values on the x-axis to plot over,
 #'    if `NULL` uses all times from `task`.
+#' @param xyline (`logical(1)`) \cr
+#'   If `TRUE` (default) plots the x-y line for `type = "dcalib"`.
+#' @param cuts (`integer(1)`) \cr
+#'   Number of cuts in [0,1] to plot `dcalib` over, default is `11`.
 #' @param ... (`any`):
 #'   Additional arguments, currently unused.
+#'
+#' @references
+#' Haider, H., Hoehn, B., Davis, S., & Greiner, R. (2020).
+#' Effective ways to build and evaluate individual survival distributions.
+#' Journal of Machine Learning Research, 21(85), 1–63.
 #'
 #' @export
 #' @examples
@@ -28,10 +41,20 @@
 #' library(mlr3viz)
 #'
 #' learn = lrn("surv.coxph")
-#' task = tsk("rats")
-#' p = learn$train(task, row_ids = 1:100)$predict(task, row_ids = 101:200)
-#' autoplot(p, type = "calib", task = task)
-autoplot.PredictionSurv = function(object, type = "calib", task = NULL, row_ids = NULL, times = NULL, ...) { # nolint
+#' task = tsk("unemployment")
+#' p = learn$train(task, row_ids = 1:300)$predict(task, row_ids = 301:400)
+#'
+#' # calibration by comparison of average prediction to Kaplan-Meier
+#' autoplot(p, type = "calib", task = task, row_ids = 301:400)
+#'
+#' # Distribution-calibration (D-Calibration)
+#' autoplot(p, type = "dcalib")
+#'
+#' @export
+autoplot.PredictionSurv = function(object, type = c("calib", "dcalib"),
+                                   task = NULL, row_ids = NULL, times = NULL, xyline = TRUE,
+                                   cuts = 11L, ...) {
+
   x = y = Group = NULL
 
   switch(type,
@@ -52,6 +75,17 @@ autoplot.PredictionSurv = function(object, type = "calib", task = NULL, row_ids 
 
       ggplot(data, aes(x = x, y = y, group = Group, color = Group)) + geom_line() +
         labs(x = "T", y = "S(T)")
+    },
+
+    "dcalib" = {
+      p = seq.int(0, 1, length.out = cuts)
+      q = sapply(p, function(.x)
+        sum(object$truth[, 1L] <= as.numeric(object$distr$quantile(.x)))/100)
+      pl = qplot(x = p, y = q, geom = "line")
+      if (xyline) {
+        pl = pl + geom_abline(slope = 1, intercept = 0, color = "red")
+      }
+      pl + labs(x = "True", y = "Predicted")
     },
 
     stopf("Unknown plot type '%s'", type)
