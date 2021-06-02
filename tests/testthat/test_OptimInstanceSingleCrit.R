fa0b07e8e4ab60ebb8e3b9a2ee1a48ca600e9233
Jakob Richter <code@jakob-r.de>
2021-06-02 12:01:16 +0200

    ...

--- tests/testthat/test_OptimInstanceSingleCrit.R
+++ tests/testthat/test_OptimInstanceSingleCrit.R
@@ -1,61 +1,74 @@
-test_that("fortify.TuningInstanceSingleCrit", {
+test_that("fortify.OptimInstanceSingleCrit", {
   skip_if_not_installed("mlr3")
   skip_if_not_installed("paradox")
-  skip_if_not_installed("mlr3tuning")
   skip_if_not_installed("patchwork")
-  skip_if_not_installed("mlr3tuning")
-  library(mlr3tuning)
+  skip_if_not_installed("bbotk")
+  library("bbotk") # nolint
+  library("paradox") # nolint
 
-  learner = mlr3::lrn("classif.rpart")
-  learner$param_set$values$cp = paradox::to_tune(0.001, 0.1)
-  learner$param_set$values$minsplit = paradox::to_tune(1, 10)
+  fun = function(xs) {
+    c(y = - (xs[[1]] - 2)^2 - (xs[[2]] + 3)^2 + 10)
+  }
+  domain = ps(
+    x1 = p_dbl(-10,10),
+    x2 = p_dbl(-5, 5)
+  )
+  codomain = ps(
+    y = p_dbl(tags = "maximize")
+  )
+  obfun = ObjectiveRFun$new(
+    fun = fun,
+    domain = domain,
+    codomain = codomain
+  )
 
-  instance = TuningInstanceSingleCrit$new(
-    task = mlr3::tsk("iris"),
-    learner = learner,
-    resampling = mlr3::rsmp("holdout"),
-    measure = mlr3::msr("classif.ce"),
-    terminator = trm("evals", n_evals = 4))
+  instance = OptimInstanceSingleCrit$new(objective = obfun, terminator = trm("evals", n_evals = 20))
 
-  tuner = tnr("random_search", batch_size = 2)
-  tuner$optimize(instance)
+  optimizer = opt("random_search", batch_size = 2)
+  optimizer$optimize(instance)
 
   f = fortify(instance)
-  expect_data_table(f, nrows = 4, ncols = 8)
-  expect_names(names(f), permutation.of = c("cp", "minsplit", "classif.ce", "uhash", "timestamp", "batch_nr",
-    "x_domain_cp", "x_domain_minsplit"))
+  expect_data_table(f, nrows = 20, ncols = 7)
+  expect_names(names(f), permutation.of = c("x1", "x2", "y", "timestamp", "batch_nr",
+                                            "x_domain_x1", "x_domain_x2"))
 })
 
-test_that("autoplot.TuningInstanceSingleCrit", {
+test_that("autoplot.OptimInstanceSingleCrit", {
   skip_if_not_installed("mlr3")
   skip_if_not_installed("paradox")
   skip_if_not_installed("mlr3learners")
-  skip_if_not_installed("mlr3tuning")
   skip_if_not_installed("ranger")
-  skip_if_not_installed("xgboost")
   skip_if_not_installed("patchwork")
-  requireNamespace("mlr3learners") # nolint
-  library("mlr3tuning") # nolint
+  skip_if_not_installed("bbotk")
+  library("bbotk") # nolint
+  library("paradox") # nolint
 
-  learner = mlr3::lrn("classif.rpart")
-  learner$param_set$values$cp = paradox::to_tune(0.001, 0.1)
-  learner$param_set$values$minsplit = paradox::to_tune(1, 10)
+  fun = function(xs) {
+    c(y = - (xs[[1]] - 2)^2 - (xs[[2]] + 3)^2 + 10)
+  }
+  domain = ps(
+    x1 = p_dbl(-10,10),
+    x2 = p_dbl(-5, 5)
+  )
+  codomain = ps(
+    y = p_dbl(tags = "maximize")
+  )
+  obfun = ObjectiveRFun$new(
+    fun = fun,
+    domain = domain,
+    codomain = codomain
+  )
 
-  instance = TuningInstanceSingleCrit$new(
-    task = mlr3::tsk("iris"),
-    learner = learner,
-    resampling = mlr3::rsmp("holdout"),
-    measure = mlr3::msr("classif.ce"),
-    terminator = trm("evals", n_evals = 4))
+  instance = OptimInstanceSingleCrit$new(objective = obfun, terminator = trm("evals", n_evals = 20))
 
-  tuner = tnr("random_search", batch_size = 2)
-  tuner$optimize(instance)
+  optimizer = opt("random_search", batch_size = 2)
+  optimizer$optimize(instance)
 
   p = autoplot(instance, type = "marginal")
   expect_class(p, "DelayedPatchworkPlot")
   lapply(p, function(x) expect_true(is.ggplot(x)))
 
-  p = autoplot(instance, type = "marginal", cols_x = "x_domain_cp")
+  p = autoplot(instance, type = "marginal", cols_x = "x_domain_x1")
   expect_class(p, "DelayedPatchworkPlot")
   lapply(p, function(x) expect_true(is.ggplot(x)))
 
@@ -74,7 +87,7 @@ test_that("autoplot.TuningInstanceSingleCrit", {
   expect_class(p, "DelayedPatchworkPlot")
   lapply(p, function(x) expect_true(is.ggplot(x)))
 
-  p = autoplot(instance, type = "parameter", cols_x = "x_domain_cp")
+  p = autoplot(instance, type = "parameter", cols_x = "x_domain_x1")
   expect_class(p, "DelayedPatchworkPlot")
   lapply(p, function(x) expect_true(is.ggplot(x)))
 
@@ -86,15 +99,6 @@ test_that("autoplot.TuningInstanceSingleCrit", {
   expect_class(p, "DelayedPatchworkPlot")
   lapply(p, function(x) expect_true(is.ggplot(x)))
 
-  p = autoplot(instance, type = "surface")
-  expect_true(is.ggplot(p))
-
-  p = autoplot(instance, type = "surface", grid_resolution = 50)
-  expect_true(is.ggplot(p))
-
-  p = autoplot(instance, type = "surface", learner = lrn("regr.lm"))
-  expect_true(is.ggplot(p))
-
   p = autoplot(instance, type = "points")
   expect_true(is.ggplot(p))
 
@@ -104,41 +108,5 @@ test_that("autoplot.TuningInstanceSingleCrit", {
   p = autoplot(instance, type = "pairs")
   expect_s3_class(p, "ggmatrix")
 
-  # with categoircal params
-
-  learner = mlr3::lrn("classif.xgboost")
-  learner$param_set$values$eta = paradox::to_tune(0.01, 0.1)
-  learner$param_set$values$nrounds = paradox::to_tune(1, 2)
-  learner$param_set$values$booster = paradox::to_tune()
-  learner$param_set$values$maximize = paradox::to_tune()
-
-  instance = TuningInstanceSingleCrit$new(
-    task = mlr3::tsk("iris"),
-    learner = learner,
-    resampling = mlr3::rsmp("holdout"),
-    measure = mlr3::msr("classif.ce"),
-    terminator = trm("evals", n_evals = 4))
-
-  tuner = tnr("random_search", batch_size = 2)
-  tuner$optimize(instance)
-
-  p = autoplot(instance, type = "parallel")
-  expect_true(is.ggplot(p))
-
-  p = autoplot(instance, type = "parallel", cols_x = c("maximize", "booster"))
-  expect_true(is.ggplot(p))
-
-  expect_error(autoplot(instance, type = "surface"),
-    regexp = "Surface plots can only be drawn with 2 parameters.",
-    fixed = TRUE)
-
-  expect_error(autoplot(instance, type = "surface", cols_x = "nrounds"),
-    regexp = "Surface plots can only be drawn with 2 parameters.",
-    fixed = TRUE)
-
-  instance$archive$data[1, 1] = NA
-
-  expect_error(autoplot(instance, type = "parallel"),
-    regexp = "Parallel coordinate plots cannot be displayed with missing data.",
-    fixed = TRUE)
+  # categorical domain is tested through test_TuningInstanceSingleCrit
 })
