0a87ed19ec0fb7cf6ebd5020dcc6737a66e6a157
be-marc <marcbecker@posteo.de>
2023-11-22 09:43:12 +0100

    fix: skip cluster learner

--- tests/testthat/test_LearnerClustHierarchical.R
+++ tests/testthat/test_LearnerClustHierarchical.R
@@ -4,6 +4,7 @@ skip_if_not_installed("ggdendro")
 set.seed(42)
 
 test_that("autoplot.LearnerClustHierarchical", {
+  skip_on_cran()
   require_namespaces("mlr3cluster")
 
   learner = mlr3::lrn("clust.agnes")$train(mlr3::tsk("usarrests"))
