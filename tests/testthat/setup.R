a1ee48b9adf319e40f725b2e78b196fcb029499b
be-marc <marcbecker@posteo.de>
2023-11-23 12:51:46 +0100

    fix: set threads in tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,3 +3,9 @@ if (requireNamespace("lgr")) {
   old_threshold = lg$threshold
   lg$set_threshold("warn")
 }
+
+old_dt_threads = data.table::getDTthreads()
+old_omp_threads = Sys.getenv("OMP_THREAD_LIMIT")
+
+setDTthreads(1)
+Sys.setenv("OMP_THREAD_LIMIT" = 1)
